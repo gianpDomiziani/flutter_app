420a72825705fc99672640fc3e2a5550c792c854
gianpDomiziani <gpdomiziani@gmail.com>
2021-06-07 13:48:35 +0200

    expense planner app started.

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,42 @@
 import 'package:flutter/material.dart';
 
-import './quiz.dart';
-import './result.dart';
-
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  final _questions = const [
-    {
-      'questionText': 'What\'s your preferit color?',
-      'answers': [
-        {'text': 'Black', 'score': 0},
-        {'text': 'Green', 'score': 3},
-        {'text': 'White', 'score': 1},
-        {'text': 'Blue', 'score': 2}
-      ],
-    },
-    {
-      'questionText': 'What\'s your preferit animal?',
-      'answers': [
-        {'text': 'Dog', 'score': 1},
-        {'text': 'Cat', 'score': 1},
-        {'text': 'Elephant', 'score': 1},
-        {'text': 'Lion', 'score': 1}
-      ],
-    },
-    {
-      'questionText': 'What\'s your preferit name?',
-      'answers': [
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
-      ],
-    },
-  ];
-  var _questionIndex = 0;
-  var _totalScore = 0;
-
-  void _resetQuiz() {
-    setState(() {
-      _questionIndex = 0;
-      _totalScore = 0;
-    });
-  }
-
-  void _answerQuestion(int score) {
-    _totalScore += score;
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-
-    if (_questionIndex > _questions.length) {
-      print('we have more questions');
-    } else {
-      print('We have no more questions.');
-    }
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext ctx) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.lightBlue,
-          title: Text(
-            'Personality Quiz',
-          ),
-        ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                questions: _questions,
-                questionIndex: _questionIndex,
-                answerQuestion: _answerQuestion)
-            : Result(_totalScore, _resetQuiz),
-      ),
+      title: 'Expense Planner',
+      home: MyHomePage(),
     );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctx) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Expense Planner App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              color: Colors.lightBlue,
+              width: double.infinity,
+              child: Card(
+                child: Text('Charts!'),
+                elevation: 5,
+              ),
+            ),
+            Container(
+              color: Colors.lightBlue,
+              width: double.infinity,
+              child: Card(
+                child: Text('List of txs'),
+              ),
+            )
+          ],
+        ));
   }
 }
